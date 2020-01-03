e53d37c4a717277d1ef2353d7677c54f654e2314
flash <nobless@yandex.ru>
2020-01-04 03:35:27 +0500

    Exercise three complete.

---
+++ ex03_conditions.py
@@ -0,0 +1,10 @@
+num = int (input ("Input any num:"))
+if num > 0:
+    print ("Number is bigger zero")
+elif num < 0:
+    print ("Number is less zero")
+else:
+    print ("Number is zero")
+del num
+line = input ("Input any text:")
+print ("Funny." if line == "any text" else "Ok.")
